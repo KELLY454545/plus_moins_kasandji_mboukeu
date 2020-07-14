0e083fc0182509efb05b5e7f5862e436506e6961
KELLY <kimpopico10@gmail.com>
2020-07-14 02:23:11 -0400

    nouveau code plus_moins.c

--- plus_moins.c
+++ plus_moins.c
@@ -42,39 +42,50 @@ int plus_moins (int value)
 int main()
 {
 
-        int valeur_trouver = 0, choix_joueur = 0;
+        int valeur_trouver = 0, choix_joueur = 0, difficulte = 0;
         int nombreTape =0, rejouer=0, coups =0, nombres_essaie =0, modeJeu =0, niveauJeu =0;
 	const int MAX = 100, MIN = 1;
 	srand(time(NULL));
-	valeur_trouver = (rand() % (MAX - MIN + 1)) + MIN;
+        
+        printf("===Niveau De Difficulte===\n");
+        printf("1. Niveau facile : Entre 1 et 100\n");
+        printf("2. Niveau moyen : Entre 1 et 1000\n");
+        printf("3. Niveau difficile : Entre 1 et 10000\n");
+        scanf("%d", &difficulte);
+        if (difficulte == 1)
+           {
+              int MAX = 100;
+	      valeur_trouver = (rand() % (MAX - MIN + 1)) + MIN;
+	      nombres_essaie =1000;
+	      nombres_essaie--;
+	      return plus_moins( valeur_trouver);
 
-	return plus_moins( valeur_trouver);
-        if (modeJeu == 1)
-              {
-	          printf ("Choisissez un niveau ! :\n1 = Facile\n2 = Moyen\n3 = Difficile\n")
+
+           }
+        else if (difficulte == 2)
+           {
+              int MAX = 1000;
+	      valeur_trouver = (rand() % (MAX - MIN + 1)) + MIN;
+	      nombres_essaie = 25;
+	      nombres_essaie--;
+	      return plus_moins( valeur_trouver);
 
 
-                  do     {
-				printf ("Votre choix ? ");
-				scanf ("%d", &niveauJeu);
-				printf ("Vous avez choisis le niveau %d\n\n", niveauJeu);
-			  }switch (niveauJeu );
-                                   {
-				        case 1:
-					          Max = 100;
-					          valeur_trouver = (rand() %(nombreMax - MIN + 1) ) + MIN;
-					          coupsRestant = 200;
-					break;
-				        case 2:
-					          Max = 600;
-					          valeur_trouver = (rand() %(nombreMax - MIN + 1) ) + MIN;
-					          nombres_essaie = 25;
-					break;
-				        case 3:
-					          Max = 1100;
-					          valeur_trouver = (rand() %(nombreMax - MIN + 1) ) + MIN;
-					          nombres_essaie = 10;
-					break;     }}
+           }
+         else if (difficulte == 3)
+           {
+              int MAX = 10000;
+	      valeur_trouver = (rand() % (MAX - MIN + 1)) + MIN;
+	      nombres_essaie = 10;
+	      nombres_essaie--;
+	      return plus_moins( valeur_trouver);
+
+
+           }
+         else
+           {
+              printf("Vous vous êtes trompé de touche\n petit con\n");
+           }
 } 
 
 
