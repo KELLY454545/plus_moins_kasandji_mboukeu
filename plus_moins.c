384d9f73f0c19048f880f516480bc2a0d24ecf9d
KELLY <kimpopico10@gmail.com>
2020-07-16 04:10:30 -0400

    plus_moins.c

--- plus_moins.c
+++ plus_moins.c
@@ -6,7 +6,7 @@
 
 int main()
 {
-    int nombreMystere = 0, nombreEntre = 0;
+    int nombre_a_deviner = 0, nombreEntre = 0;
     int compteurDeCoups = 0;
     int continuerPartie = 1;
     int modeDuJeu = 0;
@@ -16,10 +16,13 @@ int main()
 
     //Initialise le générateur de nombres aléatoires
     srand(time(NULL));
+    
+    nombre_a_deviner = (rand() % (max - min + 1)) + min;
+
 
 
     /* La boucle du programme. Elle se répète tant que l'utilisateur
-    n'a pas trouvé le nombre mystère */
+    n'a pas trouvé le nombre a deviner*/
 
     while(continuerPartie!=0)
     {
@@ -43,48 +46,67 @@ int main()
         }
 
         if(niveauDeDifficulte==1 || niveauDeDifficulte == 2 || niveauDeDifficulte == 3)
+	printf("vous avez 6 essaie ? ");
+
         {
-            // Génération du nombre aléatoire
-            nombreMystere = (rand() % (max - min + 1)) + min;
 
             switch(modeDuJeu)
             {
+	        
+
                 case 1 /*Mode 1 joueur*/:
                             do
                             {
+				//Initialise le générateur de nombres aléatoires
+
+			        nombre_a_deviner = (rand() % (max - min + 1)) + min;
+
                                 //Compteur de coups
-                                compteurDeCoups++;
+                                compteurDeCoups++ ;
                                 // On demande le nombre
                                 printf("Quel est le nombre ? ");
                                 scanf("%d", &nombreEntre);
 
                                 // On compare le nombre entré avec le nombre mystère
 
-                                if (nombreMystere > nombreEntre)
+                                if (nombre_a_deviner > nombreEntre)
                                     printf("C'est plus !\n\n");
-                                else if (nombreMystere < nombreEntre)
+                                else if (nombre_a_deviner < nombreEntre)
                                     printf("C'est moins !\n\n");
                                 else
                                     printf ("Bravo, vous avez trouve le nombre mystere en %d coups !!!\n\n",compteurDeCoups);
 
-                            } while (nombreEntre != nombreMystere);break;
+                            } while (nombreEntre != nombre_a_deviner);break;
                 case 2 /*Mode 2 joueur*/:
                     do
                     {
                         //Compteur de coups
-                        compteurDeCoups++;
+
+	
+
+                        for(int compteurDeCoups=0; compteurDeCoups < 2; compteurDeCoups++)
+		
                         // On demande le nombre
                         if(finDuJeu!=0)
                         {
 
-                            printf("Joueur 1 : Quel est le nombre ? ");
-                            scanf("%d", &nombreEntre);
+			    nombre_a_deviner = (rand() % (max - min + 1)) + min;
 
-                            // On compare le nombre entré avec le nombre mystère
+                            
 
-                            if (nombreMystere > nombreEntre)
+
+			     printf("Joueur 2 : Quel est le nombre ? ");
+			     scanf("%d", &nombreEntre);
+
+
+
+
+
+                            // On compare le nombre entré avec le nombre a diviner
+
+                            if (nombre_a_deviner > nombreEntre)
                                 printf("C'est plus !\n\n");
-                            else if (nombreMystere < nombreEntre)
+                            else if (nombre_a_deviner < nombreEntre)
                                 printf("C'est moins !\n\n");
                             else
                             {
@@ -92,34 +114,16 @@ int main()
                                 finDuJeu = 0;
                             }
                         }
-                        if(finDuJeu!=0)
-                        {
 
-                            printf("Joueur 2 : Quel est le nombre ? ");
-                            scanf("%d", &nombreEntre);
-
-                            // On compare le nombre entré avec le nombre mystère
-
-                            if (nombreMystere > nombreEntre)
-                                printf("C'est plus !\n\n");
-                            else if (nombreMystere < nombreEntre)
-                                printf("C'est moins !\n\n");
-                            else
-                            {
-                                printf ("Bravo, vous avez trouve le nombre mystere en %d coups !!!\n\n",compteurDeCoups);
-                            }
-                        }
-
-                    } while (nombreEntre != nombreMystere);break;
+                    } while (nombreEntre != nombre_a_deviner);break;
                 default : printf("Mode du joueur errone, veuillez choisir 1 ou 2!\n");
             }
         }
 
-        printf("Voulez-vous faire une autre partie(tapez 1 pour Oui, tapez 0 pour Non)? ");
-        scanf("%d", &continuerPartie);
+        
     }
 
-    printf("\t*****************GAME OVER*****************\n");
+   
 
     return 0;
 }
